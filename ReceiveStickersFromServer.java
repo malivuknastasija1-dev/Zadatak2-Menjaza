b2b958d605e6ceb743a0f74b19c21ca27438afb8
malivuknastasija1-dev <malivuknastasija1@gmail.com>
2026-06-06 07:18:10 +0200

    Update ReceiveStickersFromServer.java

--- ReceiveStickersFromServer.java
+++ ReceiveStickersFromServer.java
@@ -1,4 +1,5 @@
 package com.mycompany.menjazaclient;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -21,13 +22,11 @@ public class ReceiveStickersFromServer implements Runnable {
             try{
                 line = this.br.readLine();
                 if (line != null){
-                    // Odkomentarisano radi lakšeg praćenja rada mrežnog koda
-//                    System.out.println("KLIJENT PRIMIO: " + line);
                     
                     if (line.startsWith("INITIAL_SET;")){
                         String[] tokeni = line.split(";");
                         String allDuplicates = "Nema duplikata";
-                        String allMissing = "Nema slicica koje nedostaju";
+                        String allMissing = "Nema sličica koje nedostaju";
                         
                         String[] duplicateTokeni = tokeni[1].split(":");
                         if (duplicateTokeni.length > 1){
@@ -40,17 +39,17 @@ public class ReceiveStickersFromServer implements Runnable {
                         }
 
                         if (!parent.isUputstvoPrikazano()){
-                            String uputstvo = "UPUTSTVO ZA MENJAZU SLICICA: \n" + 
-                                "1. Stiklirajte SAMO duplikate koje zelite da PONUDITE.\n" + 
-                                "2. Stiklirajte SAMO odredjene slicice koje TRAZITE.\n" + 
-                                "Ako ne stiklirate NISTA - nudite sve duplikate i trazite sve koje Vam fale!\n";
+                            String uputstvo = "UPUTSTVO ZA MENJAžU SLIčICA: \n" + 
+                                "1. Štiklirajte SAMO duplikate koje želite da PONUDITE.\n" + 
+                                "2. Štiklirajte SAMO određene sličice koje TRAŽITE.\n" + 
+                                "Ako ne štiklirate NIŠTA - server sam izabere koje ćete ponuditi i potražiti!\n";
                             javax.swing.JOptionPane.showMessageDialog(parent, uputstvo, "Uputstvo", javax.swing.JOptionPane.INFORMATION_MESSAGE);
                             parent.setUputstvoPrikazano(true);
                         }
                         
                         parent.getTaConsole().append("*** VASE SLICICE ***\n");
                         parent.getTaConsole().append("Duplikati za razmenu: " + allDuplicates + "\n");
-                        parent.getTaConsole().append("Slicice koje Vam nedostaju: " + allMissing + "\n"); 
+                        parent.getTaConsole().append("Sličice koje Vam nedostaju: " + allMissing + "\n"); 
                         parent.getTaConsole().append("--------------------------------------------------\n");
                         
                         if (duplicateTokeni.length > 1 && !allDuplicates.equals("Nema duplikata")) {
@@ -60,7 +59,7 @@ public class ReceiveStickersFromServer implements Runnable {
                             parent.updateDuplicates(new String[0]);
                         }
 
-                        if (missingTokeni.length > 1 && !allMissing.equals("Nema slicica koje nedostaju")) {
+                        if (missingTokeni.length > 1 && !allMissing.equals("Nema sličica koje nedostaju")) {
                             String[] nizBrojevaNedostaje = allMissing.split(",");
                             parent.updateMissing(nizBrojevaNedostaje);
                         } else {
@@ -99,31 +98,31 @@ public class ReceiveStickersFromServer implements Runnable {
                                                 parent.getCbPlayers().addItem(displayComboBox);
                                                 numOtherPlayers++;
                                                 
-                                                parent.getTaConsole().append("Mozes da menjas slicice sa korisnikom " + name + ".\n");
+                                                parent.getTaConsole().append("Možeš da menjaš sličice sa korisnikom " + name + ".\n");
                                                 if (data.length > 1 && !data[1].equals("Nista")) {
-                                                    parent.getTaConsole().append("  -> Igrac tebi moze dati: " + data[1].replace(",", ", ") + "\n");
+                                                    parent.getTaConsole().append("  -> Igrač tebi može dati: " + data[1].replace(",", ", ") + "\n");
                                                 }
                                                 if (data.length > 2 && !data[2].equals("Nista")) {
-                                                    parent.getTaConsole().append("  -> Ti igracu mozes dati: " + data[2].replace(",", ", ") + "\n");
+                                                    parent.getTaConsole().append("  -> Ti igraču možeš dati: " + data[2].replace(",", ", ") + "\n");
                                                 }
-                                                parent.getTaConsole().append("  Mogucih obostranih razmena: " + data[0] + "\n");
+                                                parent.getTaConsole().append("  Mogućih obostranih razmena: " + data[0] + "\n");
                                                 parent.getTaConsole().append("----------------------------------------\n");
                                             }
                                         }
                                     }
                                     
                                     if (numOtherPlayers == 0) {
-                                        parent.getCbPlayers().addItem("Nema drugih aktivnih igraca");
+                                        parent.getCbPlayers().addItem("Nema drugih aktivnih igrača");
                                     }
                                     
                                     parent.getCbPlayers().setEnabled(true);
                                     parent.getCbPlayers().revalidate();
                                     parent.getCbPlayers().repaint();
                                     
-                                    System.out.println("Uspešno osvezeno! Broj stavki u ComboBox-u: " + parent.getCbPlayers().getItemCount());
+                                    System.out.println("Uspešno osveženo! Broj stavki u ComboBox-u: " + parent.getCbPlayers().getItemCount());
                                     
                                 } catch (Exception problem) {
-                                    System.out.println("GRESKA PRI PARSIRANJU LISTE: " + problem.getMessage());
+                                    System.out.println("Greška pri parsiranju liste: " + problem.getMessage());
                                     problem.printStackTrace();
                                 }
                             }
@@ -134,6 +133,7 @@ public class ReceiveStickersFromServer implements Runnable {
                         String from = "";
                         String offer = "";
                         String require = "";
+                        
                         for(String tok : tokeni){
                             if (tok.startsWith("from:")) from = tok.substring(5).trim();
                             if (tok.startsWith("offer:")) offer = tok.substring(6).trim();
@@ -148,7 +148,7 @@ public class ReceiveStickersFromServer implements Runnable {
                             @Override
                             public void run(){
                                 int answer = javax.swing.JOptionPane.showConfirmDialog(parent,
-                                        "Igrac " + finalFrom + " nudi slicice: [" + finalOffer + "], a trazi od Vas: [" + finalRequire + "]. Da li prihvatate razmenu?",
+                                        "Igrač " + finalFrom + " nudi sličice: [" + finalOffer + "], a traži od Vas: [" + finalRequire + "]. Da li prihvatate razmenu?",
                                         "Zahtev za razmenu", javax.swing.JOptionPane.YES_NO_OPTION);
                                 if (answer == javax.swing.JOptionPane.YES_OPTION){
                                     parent.getTaConsole().append("Prihvacena razmena sa " + finalFrom + "\n");
@@ -156,7 +156,7 @@ public class ReceiveStickersFromServer implements Runnable {
                                         java.io.PrintWriter pwFromClass = new java.io.PrintWriter(parent.getSoc().getOutputStream(), true);
                                         pwFromClass.println("EXCHANGE_ACCEPTED;peer:" + finalFrom + ";hisOffer:" + finalOffer + ";hisRequire:" + finalRequire);
                                     }catch(Exception problem){
-                                        parent.getTaConsole().append("Greska prilikom slanja potvrde serveru.\n");
+                                        parent.getTaConsole().append("Greška prilikom slanja potvrde serveru.\n");
                                     }
                                 }else{
                                     parent.getTaConsole().append("Odbijena razmena.\n");
